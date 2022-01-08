9183bc9b0c2c80a73fb48b704016e382ea725f53
welleh <kevinjiang64@hotmail.com>
2022-01-08 18:06:54 -0500

    Updates

---
+++ precache-manifest.a5e1135ab76ba1628bd4f28c60cbfa9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2217b27f959252e4d61c7089ba160bd3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3e7259d104031f22e995",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d48173b192e251cd2504",
+    "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "3e7259d104031f22e995",
+    "url": "/portfolio/static/js/2.41f3b19c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.41f3b19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48173b192e251cd2504",
+    "url": "/portfolio/static/js/main.3c5a3850.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
